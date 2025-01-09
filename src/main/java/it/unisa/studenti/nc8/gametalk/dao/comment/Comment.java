7ab5b954c8fc9510ac08aba819f7807b0472937f
CarmeloCapp <cappiello11@gmail.com>
2025-01-09 15:19:06 +0100

    Interfaccia Database e Comment

---
+++ src/main/java/it/unisa/studenti/nc8/gametalk/dao/comment/Comment.java
@@ -0,0 +1,4 @@
+package it.unisa.studenti.nc8.gametalk.dao.comment;
+
+public interface Comment {
+}
---
+++ src/main/java/it/unisa/studenti/nc8/gametalk/persistence/Database.java
@@ -0,0 +1,13 @@
+package it.unisa.studenti.nc8.gametalk.persistence;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface Database {
+    boolean connect () throws SQLException;
+    boolean isConnected () throws SQLException;
+    boolean close() throws SQLException;
+    ResultSet executeQuery(String query, Object... parameters) throws SQLException;
+    int executeUpdate(String query, Object... parameters) throws SQLException;
+    int executeUpdateReturnKeys(String query, Object... parameters) throws SQLException;
+}
