a73fef4a6010d121bf21786c85c0adf28ef13c46
Lexi115 <shinokataku1001@gmail.com>
2025-01-11 20:50:06 +0100

    Test del logging framework Log4J2

--- src/test/java/it/unisa/studenti/nc8/gametalk/MyTest.java
+++ src/test/java/it/unisa/studenti/nc8/gametalk/MyTest.java
@@ -1,11 +1,14 @@
 package it.unisa.studenti.nc8.gametalk;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class MyTest {
     private static int multiplier;
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @BeforeAll
     public static void setUp() {
@@ -20,5 +23,9 @@ public class MyTest {
     @Test
     public void multiply3() {
         Assertions.assertEquals(6, 3 * multiplier);
+    }
+
+    public static void main(String[] args) {
+        LOGGER.fatal("prova!! {} {}", 30, 20);
     }
 }
